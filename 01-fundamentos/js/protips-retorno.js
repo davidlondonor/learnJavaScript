7b973f52c1ce71b679abf1a7453533899b3f3201
davidlucky@gmail.com <davidlucky@gmail.com>
2020-03-14 20:03:48 -0500

    arrow function

---
+++ 01-fundamentos/js/protips-retorno.js
@@ -0,0 +1,57 @@
+// function crearPersona(nombre, apellido) {
+//     return { nombre, apellido };
+// }
+
+// La misma función en modo Flecha
+
+const crearPersona = (nombre, apellido) => ({ nombre, apellido });
+
+const persona = crearPersona("David", "Londoño");
+console.log(persona);
+
+function imprimeArgumento() {
+    console.log(arguments);
+}
+
+const imprimeArgumentos2 = (edad, ...args) => {
+    // console.log({ edad, args });
+    return args;
+};
+
+const [casado, vivo, nombre, saludo] = imprimeArgumentos2(
+    10,
+    true,
+    false,
+    "david",
+    "hola"
+);
+console.log({ casado, vivo, nombre, saludo });
+
+const { apellido: nuevoApellido } = crearPersona("David", "Londoño");
+console.log({ nuevoApellido });
+
+const tony = {
+    nombre: "Tony Stark",
+    codeName: "Ironman",
+    vivo: false,
+    edad: 40,
+    trajes: ["Mark I", "Mark V", "Hulkbuster"]
+};
+
+// const imprimrPropiedades = personaje => {
+//     console.log("nombre", personaje.nombre);
+//     console.log("codeName", personaje.codeName);
+//     console.log("vivo", personaje.vivo);
+//     console.log("edad", personaje.edad);
+//     console.log("trajes", personaje.trajes);
+// };
+
+const imprimrPropiedades = personaje => {
+    console.log("nombre", personaje.nombre);
+    console.log("codeName", personaje.codeName);
+    console.log("vivo", personaje.vivo);
+    console.log("edad", personaje.edad);
+    console.log("trajes", personaje.trajes);
+};
+
+imprimrPropiedades(tony);
